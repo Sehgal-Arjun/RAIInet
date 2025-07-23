f2d5bf2b88ecd34cfa0b535680cf72075cfdafb2
aayushh-patell <aayushptl2005@gmail.com>
2025-07-23 19:07:19 -0400

    correct misplaced const

--- include/link.h
+++ include/link.h
@@ -9,15 +9,16 @@ enum class LinkType;
 class Link {
 public:
     virtual ~Link() = default;
+
     virtual int getStrength() const = 0;
     virtual int getTravelDistance() const = 0;
     virtual LinkType getLinkType() const = 0;
     virtual std::pair<int, int> getLocation() const = 0;
-    virtual Player* getOwner() const = 0;
+    virtual Player* getOwner() const = 0; // non-owning
     virtual bool isInUse() const = 0;
     virtual bool isKnight() const = 0;
     virtual void setInUse(bool inUse) = 0;
-    virtual void setLocation(std::pair<int, int> loc) const = 0;
+    virtual void setLocation(std::pair<int, int> loc) = 0;
 };
 
 #endif // LINK_H 
