04483065e3ba438b60fcd23a0e4f0b0855a4c00d
aayushh-patell <aayushptl2005@gmail.com>
2025-07-23 19:10:45 -0400

    add getter methods, set default fields, and remove manual memory management in Player class

--- include/player.h
+++ include/player.h
@@ -6,22 +6,32 @@
 #include <map>
 #include <string>
 #include "link.h"
+#include "ability.h"
 
 class Player {
     private:
-        std::vector<Ability> chosenAbilities;
-        std::map<std::string, Link*> links;
-        int dataAmountDownloaded;
-        int virusAmountDownloaded;
-        std::map<Player*, std::map<std::string, Link*>> knownOpponentLinks;
-        bool hasWon;
+        std::vector<std::unique_ptr<Ability>> chosenAbilities;
+        std::map<std::string, std::unique_ptr<Link>> links;
+        std::map<Player*, std::map<std::string, std::shared_ptr<Link>>> knownOpponentLinks;
+        int dataAmountDownloaded = 0;
+        int virusAmountDownloaded = 0;
+        bool hasWon = false;
         int playerId;
 
     public:
         ~Player() = default;
         void download(Link* l);
         void printAbilities(std::ostream& out);
-        std::vector<Ability> getAbilities();
+        std::vector<Ability*> getAbilities();
+
+        int getPlayerId() { return playerId; }
+
+        const std::vector<std::unique_ptr<Ability>>& getChosenAbilities() const { return chosenAbilities; }
+        std::vector<std::unique_ptr<Ability>>& getChosenAbilities() { return chosenAbilities; }
+        const std::map<std::string, std::unique_ptr<Link>>& getLinks() const { return links; }
+        std::map<std::string, std::unique_ptr<Link>>& getLinks() { return links; }
+        const std::map<Player*, std::map<std::string, std::shared_ptr<Link>>>& getKnownOpponentLinks() const { return knownOpponentLinks; }
+        std::map<Player*, std::map<std::string, std::shared_ptr<Link>>>& getKnownOpponentLinks() { return knownOpponentLinks; }
 };
 
 #endif // PLAYER_H
