593c34624d0d9986674467243f59f61009890256
Arjun Sehgal <arjunsehgal789@gmail.com>
2025-07-24 19:42:46 -0400

    fix firewall merge conflict

--- src/firewall.cc
+++ src/firewall.cc
@@ -1,9 +1,10 @@
 #include "../include/firewall.h"
 #include "../include/tile.h"
 
-void Firewall::applyAbility(Tile& t) {
+void Firewall::applyAbility(Tile& t, Player* p) {
     // we're sure that the tile is empty, so we can turn it into a firewall
     t.enableFirewall();
+    t.setFirewallOwner(p);
 }
 
 bool Firewall::isValidUse(Tile* t) const {
