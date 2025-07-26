4ec8e5d4533e06a11d06cbc3d3dc4c4aaa4610a1
aayushh-patell <aayushptl2005@gmail.com>
2025-07-26 17:54:08 -0400

    pass controller to display

--- include/textdisplay.h
+++ include/textdisplay.h
@@ -6,12 +6,15 @@
 #include "board.h"
 #include "player.h"
 
+class Controller; // forward declaration
+
 class TextDisplay : public View {
     Board* board;
     std::vector<Player*>* players;
     Player* perspective; // whose perspective to print
+    Controller* controller; // to access current turn
 public:
-    TextDisplay(Board* b, std::vector<Player*>* ps, Player* persp);
+    TextDisplay(Board* b, std::vector<Player*>* ps, Player* persp, Controller* ctrl);
     void notifyCell(int r, int c, int change) override;
     void notifyFull() override;
     void print(std::ostream& out) override;
