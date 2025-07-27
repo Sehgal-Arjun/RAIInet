1b594fe9f9984dbbb760cfe2b0298e0883889498
aayushh-patell <aayushptl2005@gmail.com>
2025-07-27 00:19:23 -0400

    define placement and initialisation methods

--- include/board.h
+++ include/board.h
@@ -12,7 +12,9 @@ class Board : public Subject {
     std::vector<std::vector<std::unique_ptr<Tile>>> grid;
 
     public:
+        void initialise(std::vector<Player*> players);
         void initialiseBoard(std::istream& in, std::vector<Player*> players);
+        void initialiseBoard(std::istream& in, std::vector<Player*> players, const std::vector<std::string>& link1Order, const std::vector<std::string>& link2Order);
         void placeLink(Link& l, Tile* t);
         void reveal(Link* l, Player& p);
         Tile* getTileAt(int row, int col) const;
@@ -20,7 +22,9 @@ class Board : public Subject {
         int getWidth() const;
 
     private:
+        void placeLinkVectors(std::vector<std::unique_ptr<Link>>& linksP1, std::vector<std::unique_ptr<Link>>& linksP2, std::vector<Player*> players);
         std::vector<std::unique_ptr<Link>> randomiseLinks(Player* p);
+        std::vector<std::unique_ptr<Link>> createLinksFromOrder(Player* p, const std::vector<std::string>& linkOrder);
 };
 
 #endif
